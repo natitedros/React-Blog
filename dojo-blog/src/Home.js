c2cc85a80d50212483c4ca43f68e2281511b87b2
Nati_Tedros <tedros.natnael@gmail.com>
2023-08-01 16:34:27 +0300

    Added

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,21 +1,17 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-    const [btn, setBtn] = useState("HI")
-    const handleClick = () => {
-        if (btn === "HI"){
-            setBtn("BYE");
-        }
-        else{
-            setBtn("HI");
-        }
-        console.log(btn);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return ( 
         <div className="home">
-            <h2> Homepage </h2>
-            <button onClick={handleClick}>{btn}</button>
+            {/* A prop is a dynamic value data type so it can be reusable, that's why we use a curly brace to surround it. */}
+            <BlogList blogs={blogs} title="All Blogs"/>
         </div>
      );
 }
