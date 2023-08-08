20399a7a7486cc9016800efd52e11d2a6baf9c0f
Nati_Tedros <tedros.natnael@gmail.com>
2023-08-08 23:50:04 +0300

    Finished course

---
+++ dojo-blog/src/BlogDetails.js
@@ -0,0 +1,34 @@
+import { useParams, useHistory } from "react-router-dom";
+import useFetch from "./useFetch";
+
+
+const BlogDetails = () => {
+    const {id} = useParams()
+    const {data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id)
+    const history = useHistory()
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+          method: 'DELETE'
+        }).then(() => {
+          history.push('/');
+        }) 
+    }
+    
+    return ( 
+        <div className="blog-details">
+            {error && <h2>error.message</h2>}
+            {isPending && <h2>Loading...</h2>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by - {blog.author}</p>
+                    <div>{blog.body}</div>
+                    <button onClick={handleClick}>Delete</button>
+                </article>
+            )}   
+        </div>
+     );
+}
+ 
+export default BlogDetails;
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,16 @@
-import {useState} from 'react';
+
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-
+    const { data:blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
+    
     return ( 
         <div className="home">
             {/* A prop is a dynamic value data type so it can be reusable, that's why we use a curly brace to surround it. */}
-            <BlogList blogs={blogs} title="All Blogs"/>
+            {isPending && <h2>Loading...</h2>}
+            {error && <h2>{error}</h2>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
      );
 }
